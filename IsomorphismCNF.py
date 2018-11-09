fb3d4cf06b0c04c9126dc68790b6ea292a85ac7b
Kenneth <kstroup01@mail.roosevelt.edu>
2018-11-09 13:15:16 -0600

    Release the kraken.

--- IsomorphismCNF.py
+++ IsomorphismCNF.py
@@ -1,3 +1,6 @@
+from sys import argv
+import os
+
 # Import sage and suppress all the warnings
 import warnings
 
@@ -11,12 +14,13 @@ with warnings.catch_warnings():
 def read_cnf_file(filename):
     File = open(filename)
     firstLine = File.readline()
-    numRows = int(firstLine.split(" ")[0])
-    numCols = int(firstLine.split(" ")[1])
-
+    # print(firstLine.split(" "))
+    numRows = int(firstLine.split(" ")[2])
+    numCols = int(firstLine.split(" ")[4])
+    # print(numRows)
     m = Matrix(nrows=numRows * 2, ncols=numCols + numRows)
     for n, i in enumerate(File.readlines()):
-        split_line = i.split(" ")
+        split_line = i.strip().split(" ")
         # print(n, i)
         for v in split_line:
             if int(v) < 0:
@@ -30,13 +34,28 @@ def read_cnf_file(filename):
 
     return m
 
+
+
 # Read in CNF file and generate matrix
-m1 = read_cnf_file("./notebook/data/uf50-01.clean.2")
-m2 = read_cnf_file("./notebook/data/uf50-02.clean.2")
+# m1 = read_cnf_file("./notebook/data/uf50-01.clean.2")
+# m2 = read_cnf_file("./notebook/data/uf50-02.clean.2")
 
-# Generate graph from matrices
-bm1 = BipartiteGraph(m1)
-bm2 = BipartiteGraph(m2)
+# m1 = read_cnf_file(argv[1])
+# m2 = read_cnf_file(argv[2])
 
-# Test for isomorphism of CNF graphs
-print(bm1.is_isomorphic(bm2))
+cnf_files = os.listdir("./cnf/UUF50.218.1000/clean/")
+for i in range(len(cnf_files)):
+    m1 = read_cnf_file("./cnf/UUF50.218.1000/clean/" + cnf_files[i])
+    for j in range(i+1, len(cnf_files)):
+        m2 = read_cnf_file("./cnf/UUF50.218.1000/clean/" + cnf_files[j])
+
+        # Generate graph from matrices
+        bm1 = BipartiteGraph(m1)
+        bm2 = BipartiteGraph(m2)
+
+        # Test for isomorphism of CNF graphs
+        if bm1.is_isomorphic(bm2):
+            print("**********************", cnf_files[i], cnf_files[j], bm1.is_isomorphic(bm2))
+        else:
+            print(cnf_files[i], cnf_files[j], "false")
+        # print(bm1.is_isomorphic(bm2))
